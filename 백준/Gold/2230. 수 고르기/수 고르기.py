de0405096ff3d2d3368ebf0db653efd229c2b46d
CMS0503 <48785943+CMS0503@users.noreply.github.com>
2023-07-20 15:05:01 +0900

    [Gold V] Title: 수 고르기, Time: 6512 ms, Memory: 35500 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -0,0 +1,61 @@
+import sys
+
+n, m = map(int, input().split())
+
+nums = []
+
+for i in range(n):
+    nums.append(int(input()))
+
+
+nums.sort()
+
+p1_start = 0
+p2_start = 0
+ans = float('inf')
+
+
+# def solve(p1, p2):
+#     global ans
+#     if p1 >= p2 or nums[p2] - nums[p1] < m:
+#         return
+#
+#     p1_move_gap = nums[p2] - nums[p1 + 1]
+#     p2_move_gap = nums[p2 - 1] - nums[p1]
+#     # print(p1_move_gap, p2_move_gap)
+#
+#     if p2_move_gap > p1_move_gap > m:
+#         solve(p1 + 1, p2)
+#         # print(1)
+#     elif p1_move_gap > p2_move_gap > m:
+#         solve(p1, p2 - 1)
+#         # print(2)
+#     else:
+#         if p1_move_gap >= m:
+#             solve(p1 + 1, p2)
+#             solve(p1, p2 - 1)
+#             # print(3)
+#
+#     if nums[p2] - nums[p1] < ans:
+#         ans = nums[p2] - nums[p1]
+
+def solve(p1, p2):
+    global ans
+    while p1 < n and p2 < n:
+        gap = nums[p2] - nums[p1]
+
+        if gap < m:
+            p2 += 1
+        elif gap > m:
+            p1 += 1
+            if gap < ans:
+                ans = gap
+        else:
+            ans = m
+            break
+
+if __name__ == '__main__':
+    solve(p1_start, p2_start)
+    print(ans)
+
+
