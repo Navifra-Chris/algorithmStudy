cabc055f52587de6f57fa7234f14eaf5019335eb
CMS0503 <48785943+CMS0503@users.noreply.github.com>
2023-07-25 18:43:01 +0900

    [Gold IV] Title: DSLR, Time: 7516 ms, Memory: 218160 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -7,15 +7,12 @@ orders = ['D', 'S', 'L', 'R']
 def solve():
     q = deque()
     q.append((a, ''))
-    # path = ''
     visited = [0] * 10000
     visited[a] = 1
 
     while q:
         now, now_path = q.popleft()
 
-        # if now_path == 'LL':
-        #     print(f'{now_path} : {now}')
         if now == b:
             print(now_path)
             break
@@ -36,28 +33,13 @@ def solve():
                 next_path = now_path + 'S'
 
             elif i == 2 or i == 3:  # L or R
-                # d1 = now // 1000
-                # now = now % 1000
-                #
-                # d2 = now // 100
-                # now = now % 100
-                #
-                # d3 = now // 10
-                # now = now % 10
-                #
-                # d4 = now
-
                 # L
                 if i == 2:
-                    # next = d2 * 1000 + d3 * 100 + d4 * 10 + d1
-                    next = int((now % 1000) * 10 + (now / 1000))
-                    # print(d1,d2,d3,d4)
+                    next = (now % 1000) * 10 + (now // 1000)
                     next_path = now_path + 'L'
-                    # print(next, next_path)
                 # R
                 elif i == 3:
-                    # next = d4 * 1000 + d1 * 100 + d2 * 10 + d3
-                    next = int((now % 10) * 1000 + (now / 10))
+                    next = (now % 10) * 1000 + (now // 10)
                     next_path = now_path + 'R'
 
             if next is not None and visited[next] == 0:
