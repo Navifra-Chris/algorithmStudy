1dbac8720eceadaa1488bbeb5ce0ad73e79fdd8d
CMS0503 <48785943+CMS0503@users.noreply.github.com>
2023-08-14 15:17:47 +0900

    [Gold III] Title: 개구리 점프, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.py
@@ -0,0 +1,53 @@
+import sys
+
+n, q = map(int, input().split())
+
+trees = [list(map(int, input().split())) + [i+1] for i in range(n)]
+
+trees.sort()
+
+parent = [i for i in range(n+1)]
+
+def find_parent(x):
+    if parent[x] == x:
+        return x
+
+    return find_parent(parent[x])
+
+
+def union(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+def solve():
+    now_start, now_end, _, now_idx = trees[0]
+
+    for i in range(1, n):
+        next_start, next_end, _, next_idx = trees[i]
+
+        if now_start <= next_start <= now_end:
+            union(now_idx, next_idx)
+            now_start, now_end, now_idx = next_start, next_end, next_idx
+
+        else:
+            now_start, now_end, now_idx = next_start, next_end, next_idx
+
+    for _ in range(q):
+        a, b = map(int, input().split())
+        if parent[a] == parent[b]:
+            print(1)
+        else:
+            print(0)
+
+
+if __name__ == '__main__':
+    solve()
+
+
+
